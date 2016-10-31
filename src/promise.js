dffaa1b52e0c19971626b9eeae7583f6a1d6e9d2
guanbingchang <guanbingchang@wanda.cn>
2016-10-31 15:39:46 +0800

    add resolvePromise & thenable logic

--- src/promise.js
+++ src/promise.js
@@ -2,6 +2,10 @@ var STATUS_PENDING = 0,
     STATUS_RESOLVED = 1,
     STATUS_REJECTED = 2;
 
+function thenable(value){
+    return value instanceof Promise;
+}
+
 function Promise (executor){
     var self = this ;
     this.status = STATUS_PENDING;
@@ -15,6 +19,10 @@ function Promise (executor){
     
 
     function resolve(value){
+        if(thenable(value)){
+            return value.then(resolve,reject);
+        }
+
         if(self.status === STATUS_PENDING){
             self.status = STATUS_RESOLVED;
             self.data = value;
@@ -40,7 +48,30 @@ function Promise (executor){
     catch(e){
         reject(e);
     }
-} 
+}
+
+function resolvePromise(promise,x,resolve,reject){
+    var then , thenCalledOrThrow = false;
+
+    if(promise === x){
+        return reject(new TypeError('Chaining cycle detected for promise!'))
+    }
+
+    if(x instanceof Promise){
+        if(x.status === 'pending'){
+            x.then(function(value){
+                resolvePromise(promise,value,resolve,reject);
+            })
+        }
+        else{
+            x.then(resolve,reject);
+        }
+        return;
+    }
+
+    resolve(x);
+
+}
 
 Promise.prototype.then = function(onResolved, onRejected){
     var self = this, newPromise;
@@ -50,7 +81,8 @@ Promise.prototype.then = function(onResolved, onRejected){
         return newPromise = new Promise(function(resolve,reject){
             try{
                 var x = onResolved(self.data);
-                resolve(x);
+                
+                resolvePromise(newPromise,x,resolve,reject);
             }
             catch(e){
                 reject(e);
@@ -62,7 +94,8 @@ Promise.prototype.then = function(onResolved, onRejected){
         return newPromise = new Promise(function(resolve,reject){
             try{
                 var x = onRejected(self.data)
-                resolve(x);
+                //resolve(x);
+                resolvePromise(newPromise,x,resolve,reject);
             }
             catch(e){
                 reject(e);
@@ -75,7 +108,8 @@ Promise.prototype.then = function(onResolved, onRejected){
             self.onResolvedCallback.push(function(value){
                 try{
                     var x = onResolved(self.data);
-                    resolve(x);
+                    resolvePromise(newPromise,x,resolve,reject);
+                    //resolve(x);
                 }
                 catch(e){
                     reject(e);
@@ -94,4 +128,69 @@ Promise.prototype.then = function(onResolved, onRejected){
         });
     }
 }
+
+Promise.resolve = function(value){
+    return new Promise(function(resolve){
+        resolve(value);
+    })
+}
+
+Promise.reject = function(value){
+    return new Promise(function(reject){
+        reject(value);
+    })
+}
+
+Promise.all = function(executors){
+    return new Promise(function(resolve,reject){
+        var resolvedCounter = 0;
+        var promiseCount = executors.length ;
+        var resolvedValues = new Array(promiseCount);
+        for(var i =0; i<promiseCount;i++){
+            (function(i){
+                Promise.resolve(executors[i]).then(function(value){
+                    debugger;
+                    resolvedCounter ++ ;
+                    resolvedValues[i] = value;
+                    if(resolvedCounter == promiseCount){
+                        return resolve(resolvedValues);
+                    }
+                },function(reason){
+                    return reject(reason)
+                })
+            })(i)
+        }
+    })
+}
+
+/**
+var promise = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({
+            a:1
+        });
+    },1000)
+}) 
+
+var promise1 = promise.then(function(value){
+    return {
+        a:1,
+        b:2
+    }
+});
+
+promise1.then(function(value){
+    console.log(value);
+})
+
+*/
+var p2 = new Promise((resolve,reject)=>{
+    setTimeout(resolve,3000,1337)
+})
+
+
+Promise.all([p2]).then(values => { 
+  console.log(values); // [3, 1337, "foo"] 
+});
+
 module.exports = Promise;
